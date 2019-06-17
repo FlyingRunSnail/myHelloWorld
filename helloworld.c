f602056586d7d9ece45fc7ea49d6f83a565d191e
Mr Snail <huyonga@qq.com>
2019-06-17 18:55:05 +0800

    add sscanf usage for unsigned short and unsigned char

--- helloworld.c
+++ helloworld.c
@@ -2,6 +2,14 @@
 
 int main(int argc, char *argv[])
 {
+    unsigned short addr = 0x0;
+    unsigned char  val  = 0x0;
+
     printf("hello world, this is my first github project!\n");
+
+    sscanf("0x0B24, 0xC0", "0x%hx, 0x%hhx", &addr, &val);
+
+    printf("addr = 0x%04x, val = 0x%02x\n", addr, val);
+
     return 0;
 }
