b90580c5f6a527f9b4b7198b11fd08d3a64e57e5
Mr Snail <huyonga@qq.com>
2019-06-17 18:59:49 +0800

    add sscanf usage link

--- helloworld.c
+++ helloworld.c
@@ -7,6 +7,7 @@ int main(int argc, char *argv[])
 
     printf("hello world, this is my first github project!\n");
 
+    /* link: https://www.cnblogs.com/ayanmw/p/3477248.html */
     sscanf("0x0B24, 0xC0", "0x%hx, 0x%hhx", &addr, &val);
 
     printf("addr = 0x%04x, val = 0x%02x\n", addr, val);
